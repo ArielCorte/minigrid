e035432d6e1f371837f8f1f9e75f5f839d227776
Ariel Corte <cortearielformal@gmail.com>
2023-11-17 16:11:55 -0300

    flow connection

--- components/GridSystem.tsx
+++ components/GridSystem.tsx
@@ -43,8 +43,8 @@ export default function GridSystem() {
   function placeNode(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     const cursorCoords = getCursorCoords(e);
     if (nodeCoordsExists(cursorCoords)) return;
-
-    setNodes((prevNodes) => [
+    const prevNodes = nodes;
+    const newNodes = [
       ...prevNodes,
       {
         id: nodes.length.toString(),
@@ -53,15 +53,20 @@ export default function GridSystem() {
         size: 30,
         content: nodes.length.toString(),
       },
-    ]);
+    ];
 
-    updateClosestNeighbors();
-
-    console.log(nodes);
+    updateClosestNeighbors(newNodes);
+    setNodes(() => newNodes);
+    if (!canvasRef.current) return;
+    drawLines(canvasRef.current, newNodes);
   }
 
   function resetGrid() {
     setNodes([]);
+    if (!canvasRef.current) return;
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) return;
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
   }
 
   function getClosesNeighborID(
@@ -71,7 +76,9 @@ export default function GridSystem() {
     const node = nodes.find((node) => node.id === id);
     if (!node) return;
     const closestNode = nodes.reduce((prevNode, currNode) => {
+      console.log(prevNode, currNode);
       if (currNode.id === id) return prevNode;
+      if (prevNode.id === id) return currNode;
       const prevNodeDistance = Math.sqrt(
         Math.pow(prevNode.coords.x - node.coords.x, 2) +
           Math.pow(prevNode.coords.y - node.coords.y, 2)
@@ -80,52 +87,60 @@ export default function GridSystem() {
         Math.pow(currNode.coords.x - node.coords.x, 2) +
           Math.pow(currNode.coords.y - node.coords.y, 2)
       );
+      console.log(
+        prevNodeDistance < currNodeDistance ? prevNode.id : currNode.id
+      );
       return prevNodeDistance < currNodeDistance ? prevNode : currNode;
     });
     return closestNode.id;
   }
 
   function updateNode(nodeData: NodeData) {
-    setNodes((prevNodes) => {
-      const nodeIndex = prevNodes.findIndex((node) => node.id === nodeData.id);
-      const newNodes = [...prevNodes];
-      newNodes[nodeIndex] = nodeData;
+    const updatedNodes = nodes;
+    const nodeIndex = updatedNodes.findIndex((node) => node.id === nodeData.id);
+    updatedNodes[nodeIndex] = nodeData;
 
-      updateClosestNeighbors();
-      return newNodes;
-    });
-  }
-
-  function updateClosestNeighbors() {
-    setNodes((prevNodes) => {
-      const newNodes = [...prevNodes];
-      newNodes.forEach((node) => {
-        node.closestNeighborID = getClosesNeighborID(node.id, prevNodes);
-      });
-      return newNodes;
-    });
-
-    drawLines();
-  }
-
-  function drawLines() {
+    updateClosestNeighbors(updatedNodes);
+    setNodes(() => updatedNodes);
     if (!canvasRef.current) return;
-    const ctx = canvasRef.current.getContext("2d");
+    drawLines(canvasRef.current, updatedNodes);
+  }
+
+  function dragUpdateNode(nodeData: NodeData) {
+    const updatedNodes = nodes;
+    const nodeIndex = updatedNodes.findIndex((node) => node.id === nodeData.id);
+    updatedNodes[nodeIndex] = nodeData;
+
+    updateClosestNeighbors(updatedNodes);
+    if (!canvasRef.current) return;
+    drawLines(canvasRef.current, updatedNodes);
+  }
+
+  function updateClosestNeighbors(nodes: NodeData[]) {
+    nodes.forEach((node) => {
+      node.closestNeighborID = getClosesNeighborID(node.id, nodes);
+    });
+  }
+
+  function drawLines(canvas: HTMLCanvasElement, nodes: NodeData[]) {
+    const alreadyDrawn: Map<string, string> = new Map();
+    const ctx = canvas.getContext("2d");
     if (!ctx) return;
-    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     nodes.forEach((node) => {
       if (!node.closestNeighborID) return;
       const closestNeighbor = nodes.find(
         (neighbor) => neighbor.id === node.closestNeighborID
       );
       if (!closestNeighbor) return;
-      console.log(node.coords, closestNeighbor.coords);
+      if (alreadyDrawn.get(node.id) === closestNeighbor.id) return;
       ctx.beginPath();
       ctx.moveTo(node.coords.x, node.coords.y);
       ctx.lineTo(closestNeighbor.coords.x, closestNeighbor.coords.y);
       ctx.strokeStyle = "#000";
       ctx.lineWidth = 2;
       ctx.stroke();
+      alreadyDrawn.set(closestNeighbor.id, node.id);
     });
   }
 
@@ -133,7 +148,12 @@ export default function GridSystem() {
     <div className="w-screen h-screen bg-purple-100" onMouseDown={placeNode}>
       <Hud {...{ resetGrid }} />
       {nodes.map((node) => (
-        <GridNode nodeData={node} updateNodeData={updateNode} key={node.id} />
+        <GridNode
+          nodeData={node}
+          updateNodeData={(updatedNode) => updateNode(updatedNode)}
+          dragUpdateNode={(updatedNode) => dragUpdateNode(updatedNode)}
+          key={node.id}
+        />
       ))}
       <canvas className="absolute w-full h-full z-0" ref={canvasRef}></canvas>
     </div>
@@ -145,6 +165,7 @@ function Hud({ resetGrid }: { resetGrid: () => void }) {
     <div
       className="absolute top-4 left-4 p-8 pl-4 bg-blue-500 rounded-md z-50"
       onClick={(e) => e.stopPropagation()}
+      onMouseDown={(e) => e.stopPropagation()}
     >
       <button
         className="bg-blue-200 p-2 rounded-md select-none"
@@ -159,20 +180,20 @@ function Hud({ resetGrid }: { resetGrid: () => void }) {
 function GridNode({
   nodeData,
   updateNodeData,
+  dragUpdateNode,
 }: {
   nodeData: NodeData;
   updateNodeData: (nodeData: NodeData) => void;
+  dragUpdateNode: (nodeData: NodeData) => void;
 }) {
   const [isDragging, setIsDragging] = useState(false);
 
   const targetDiv = useRef<HTMLDivElement>(null);
 
   function stopDrag(e: MouseEvent) {
-    console.log("stopDrag");
     setIsDragging(false);
     window.removeEventListener("mousemove", followCursor);
     window.removeEventListener("mouseup", stopDrag);
-    console.log(e.clientX, e.clientY);
     updateNodeData({
       ...nodeData,
       coords: {
@@ -190,17 +211,23 @@ function GridNode({
   }
 
   function followCursor(e: MouseEvent) {
-    console.log(e.clientX, e.clientY);
     if (!targetDiv.current) return;
     targetDiv.current.style.left = `${e.clientX - nodeData.size / 2}px`;
     targetDiv.current.style.top = `${e.clientY - nodeData.size / 2}px`;
+    dragUpdateNode({
+      ...nodeData,
+      coords: {
+        x: e.clientX,
+        y: e.clientY,
+      },
+    });
   }
 
   return (
     <div
       ref={targetDiv}
       className={clsx(
-        "aspect-square rounded-full absolute cursor-grab flex items-center justify-center z-10",
+        "aspect-square rounded-full absolute cursor-grab flex items-center justify-center z-10 select-none",
         nodeData.color,
         isDragging && "cursor-grabbing"
       )}
